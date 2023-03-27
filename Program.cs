71039aabe43817dbe42b772ca05024acee7f53bd
Jon Cundiff <jmc9.1987@gmail.com>
2023-03-26 20:19:32 -0400

    Beginning to code: added using namespaces, changed console title, began to implement the main menu.
    Note about "main menu": I may end up putting it in its own class to call.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,27 @@
-﻿namespace LaundrySimulator
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; //may not need?
+using System.Threading.Tasks;
+
+namespace LaundrySimulator
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Title = "Laundry Simulator";
+            //want to have something that says "press any key to start"
+            //maybe have some kind of logo? like ascii or something that pulls from a text file or however it'd work
+
+            // might need this -> ? Console.ReadKey();
+
+            Console.WriteLine("Make your selection:"); //maybe make a seperate class for this that displays the main menu
+            Console.WriteLine("\n");
+            Console.WriteLine("1.) Do Your Laundry");
+            Console.WriteLine("2.) About/How to play");
+            Console.WriteLine("3.) Exit");
+            
+
         }
     }
 }
